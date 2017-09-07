56e124fe813fedaf7f1d0c0aed33138809d40cc4
Tyler Schmidt <schmidt197@gmail.com>
2017-09-06 17:07:07 -0700

    updated code based on udacity feedback. onsubmit changes to makegrid function

--- designs.js
+++ designs.js
@@ -1,8 +1,9 @@
 // I declare the table var and set it to pixel_canvas, and its a constant
 const table = document.getElementById("pixel_canvas");
 
-//use an event listener to submit. And tie that to the makeGrid function that does all the heavy lifting.
-document.getElementById("submit").addEventListener("click", function makeGrid() {
+//use an event listener to submit. This is the makeGrid function that does all the heavy lifting.
+document.getElementById("sizePicker").onsubmit = function makeGrid() {
+    event.preventDefault();
 
     //clears the table on click.
     table.innerHTML = "";
@@ -13,7 +14,6 @@ document.getElementById("submit").addEventListener("click", function makeGrid() 
     const height = document.getElementById("input_height").value;
 
     // dynamically Build a new grid with a for loop and nested while loop.
-
     for (let i = 0; i < height; i++) {
         const rows = document.createElement("tr");
 
@@ -30,7 +30,7 @@ document.getElementById("submit").addEventListener("click", function makeGrid() 
         table.appendChild(rows);
     }
     associateCellEvents();
-});
+};
 
 //Traverse the table and call the changeColour function on the table cells
 const associateCellEvents = () => {
@@ -47,8 +47,4 @@ const changeColour = cell => {
     const colour = document.getElementById("colorPicker").value;
     //assigns cell value based on colorPicker color value
     cell.style.backgroundColor = colour;
-};
-// require width value or it returns a warning.
-const validateForm = () => {
-    return false;
 };
